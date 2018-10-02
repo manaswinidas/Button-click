779eb9cd65ef0420b463735d5957d87cd036b832
goofy3 <aquibbaig97@gmail.com>
2018-10-03 02:40:54 +0530

    added user info

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ const path = require('path');
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
+var fs = require('fs');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 let clicks = 0;
+let userlength = 0;
 
-io.on('connection', function(socket){
+io.on('connect', function(socket){
+  userlength++;
   socket.on('click', function(){
     console.log('Clicked');
     io.emit('clickChanged', ++clicks);
+    io.emit('users', userlength);
   });
 });
+
+io.on('disconnect', (socket) => {
+  userlength--;
+  console.log(userlength);
+})
+
 
 server.listen(3000, () => {
 	console.log('listening on *:3000');
